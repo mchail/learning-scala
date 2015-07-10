0c761a7348f7dc88baadd82313dabe829dc7fdb6
Steve McHail <mchail@gmail.com>
2015-07-09 21:03:51 -0700

    chained type definitions

--- 20150709/Moments.scala
+++ 20150709/Moments.scala
@@ -3,33 +3,27 @@ import scala.io.Source
 object Moments extends App {
 
 	type doubles = List[Double]
+	type doubleFn = doubles => Double
+	type doubleFnFn = (doubles, doubleFn) => Double
 
 	def mean(nums: doubles) : Double = {
 		nums.sum / nums.size
 	}
 
-	def variance(nums: doubles, meanFn: doubles => Double) : Double = {
+	def variance(nums: doubles, meanFn: doubleFn) : Double = {
 		val mu = meanFn(nums)
 
 		meanFn(nums.map{x => math.pow(x - mu, 2)})
 	}
 
-	def skewness(
-		nums: doubles,
-		meanFn: doubles => Double,
-		varianceFn: (doubles, (doubles => Double)) => Double
-	) : Double = {
+	def skewness(nums: doubles, meanFn: doubleFn, varianceFn: doubleFnFn) : Double = {
 		val mu = meanFn(nums)
 		val delta = varianceFn(nums, meanFn)
 
 		meanFn(nums.map{x => math.pow((x - mu) / delta, 3)})
 	}
 
-	def kurtosis(
-		nums: doubles,
-		meanFn: doubles => Double,
-		varianceFn: (doubles, (doubles => Double)) => Double
-	) : Double = {
+	def kurtosis(nums: doubles, meanFn: doubleFn, varianceFn: doubleFnFn) : Double = {
 		val mu = meanFn(nums)
 		val delta = varianceFn(nums, meanFn)
 
